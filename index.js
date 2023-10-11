edf097f128016b93db9ff63344c96fe8ed14a4fa
Piotr Groblewski <groblewskip@gmail.com>
2023-10-11 14:44:21 +0200

    2nd attempt

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT||8080;
 
 app.use(express.json());
 app.use(cors());
-app.use('/tractors', tractorsRoutes);
+app.use('/tractors',cors(), tractorsRoutes);
 
 
 app.use(errorHandler);
