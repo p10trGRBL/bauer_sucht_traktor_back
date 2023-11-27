4a533df44f55e166de767e0d7f14595c9f98484f
Piotr Groblewski <groblewskip@gmail.com>
2023-11-27 16:03:26 +0100

    cors check

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT||8080;
 
 
 
-app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+app.use(cors());
+//{origin: 'http://localhost:5173', credentials: true}
 //app.use(cors({origin: 'https://production-website.com' }));
 
 app.use(express.json());
