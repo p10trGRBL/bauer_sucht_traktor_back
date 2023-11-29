c75bd60012f49768aabed963515395e6f91f40e4
Piotr Groblewski <groblewskip@gmail.com>
2023-11-29 18:49:32 +0100

    fix?

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bauer-sucht-traktor.netlify.app/",
+      "https://bauer-sucht-traktor.netlify.app",
     ],
     credentials: true,
   })
