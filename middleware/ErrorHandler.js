6bf28a431eed3b645eca5ba06198017d0a6c21ef
Piotr Groblewski <groblewskip@gmail.com>
2023-12-03 22:08:47 +0100

    error consistency

--- middleware/ErrorHandler.js
+++ middleware/ErrorHandler.js
@@ -1,6 +1,7 @@
  const errorHandler = (error, req, res, next) => {
     const statusCode = error.statusCode || 500;
     const message = error.message || 'Server Error';
+    const errorMessage = error.response?.data?.error || message;
     res.status(statusCode).json({message: message});
  };
  export default errorHandler;
